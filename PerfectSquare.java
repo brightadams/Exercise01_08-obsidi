61d71c7d6204bed904e459abd62fc8a94e51c04a
Bright Ofosu Adams <47488113+brightadams@users.noreply.github.com>
2026-07-14 22:41:09 -0300

    Perfect Square algorithm debugged

--- PerfectSquare.java
+++ PerfectSquare.java
@@ -9,11 +9,22 @@ public class PerfectSquare {
 	}
 	
 	public static boolean isPerfectSquare(int num) {
-        for(int i = 1; i < num; i++) {
-        	if(i*i = num) 
+        // A negative number can never be a perfect square so we return false
+		if (num < 0){
+			return false;
+		}
+		// 0 and 1 are perfect squares so we do not need to go through the algorithm.
+		if (num == 0 || num == 1){
+			return true;
+		}
+		// The square root of any number > 2 is at most num/2 so we do not have to loop more than half of that number
+		for(int i = 1; i <= num/2; i++) {
+        	if(i*i == num)
         		return true;
         	else if (i*i > num) return false;
         }
+		// if we finish the loop then it means it is not a perfect square so we return falase
+		return false;
     }
 
 }
